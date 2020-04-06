153258b2692c05dd8b2fc2222823d4cf06abb9bc
NaChOoV <37646908+NaChOoV@users.noreply.github.com>
2020-04-05 20:40:35 -0400

    test(Login/Register): Testing for Register and Login user

--- app/src/test/java/cl/ucn/disc/dsm/fuenz/chatdisc/service/ConversationServiceTest.java
+++ app/src/test/java/cl/ucn/disc/dsm/fuenz/chatdisc/service/ConversationServiceTest.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2020. This code is purely educational, the rights of use are
- *  reserved, the owner of the code is Alvaro Castillo Calabacero
- *  contact alvarolucascc96@gmail.com
+ *  reserved, the owner of the code is Ignacio Fuenzalida Veas
+ *  contact ignacio.fuenzalida@alumnos.ucn.cl
  *  Do not use in production.
  */
 
@@ -10,7 +10,10 @@ package cl.ucn.disc.dsm.fuenz.chatdisc.service;
 import cl.ucn.disc.dsm.fuenz.chatdisc.repository.service.ConversationService;
 import cl.ucn.disc.dsm.fuenz.chatdisc.repository.service.api.MessageReceivedApiService;
 import cl.ucn.disc.dsm.fuenz.chatdisc.repository.service.model.Conversation;
+
+import cl.ucn.disc.dsm.fuenz.chatdisc.viewmodel.service.ConnectionHandler;
 import java.util.List;
+import kotlin.Triple;
 import org.junit.jupiter.api.Assertions;
 
 import org.slf4j.Logger;
@@ -19,35 +22,61 @@ import org.junit.jupiter.api.Test;
 
 
 public class ConversationServiceTest {
-  /**
-   * The Logger.
-   */
-  private static final Logger log = LoggerFactory.getLogger(ConversationServiceTest.class);
+
 
   /**
    * Test {@link ConversationService#getConversations (int)} with NewsAPI.org
    */
   @Test
-  public void testGetNoticiasNewsApi() {
+  public void testGetConversations() {
 
-    final int size = 1;
-
-    log.debug("Testing the NewsApiNoticiaService, requesting {} News.", 1);
-
-    // The noticia service
-    final ConversationService noticiaService = new MessageReceivedApiService();
-
-    // The List of Noticia.
-    final List<Conversation> noticias = noticiaService.getConversations(1);
-
-    Assertions.assertNotNull(noticias);
-    Assertions.assertEquals(noticias.size(), size, "Error de tamanio");
-
-    for (final Conversation noticia : noticias) {
-      log.debug("Conversation: {}.", noticia);
-    }
-
-    log.debug("Done.");
 
   }
+
+  /**
+   *
+   */
+  @Test
+  public void TestRegister(){
+    //Register perfect
+    String user ="usrtest";
+    String password = "pwtest";
+    String email = "emailtest@gmail.com";
+    ConnectionHandler connectionHandler = new ConnectionHandler();
+    final ConversationService connectionService = new MessageReceivedApiService();
+    int code =connectionHandler.registerHandler(email,user,password);
+    Assertions.assertEquals(0,code);
+
+    //Register with same email, and diferent user
+    user ="userTest";
+    code =connectionHandler.registerHandler(email,user,password);
+    Assertions.assertEquals(1,code);
+
+    //Register with same user, diferent email
+    user ="usrtest";
+    email ="emailtest2@gmail.com";
+    code =connectionHandler.registerHandler(email,user,password);
+    Assertions.assertEquals(2,code);
+
+  }
+
+  @Test
+  public void TestLogin(){
+    //correct login
+    String password = "yo";
+    String email = "alvarolucascc96@gmail.com";
+    ConnectionHandler connectionHandler = new ConnectionHandler();
+    final ConversationService connectionService = new MessageReceivedApiService();
+    Triple<Integer,Integer,String> triple =connectionHandler.loginHandler(email,password);
+    int code = triple.getFirst();
+    Assertions.assertEquals(0,code);
+
+    //invalid credentials
+    password = "yo1";
+    triple =connectionHandler.loginHandler(email,password);
+    code = triple.getFirst();
+    Assertions.assertEquals(1,code);
+  }
 }
+
+
